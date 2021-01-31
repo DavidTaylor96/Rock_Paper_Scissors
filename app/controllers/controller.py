af84515b46aabcd59cc3a6c551940d003d36d6a1
DavidTaylor96 <david_taylor96@outlook.com>
2021-01-31 13:40:06 +0000

    Man This is hard

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -5,51 +5,42 @@ from app.models.player import *
 
 @app.route('/')
 def index():
-  return render_template('index.html', title='Home', game1=player_1, game2=player_2)
+  return render_template('index.html', title='Home', games = player)
 
 @app.route('/', methods=["POST"])
 def add_player1():
-  game1_name = request.form["name1"]
-  game1_choice = request.form["choice1"]
-  new_player = Player1(game1_name, game1_choice)
-
-  player_1.append(new_player)
-  print('HHHH', str(player_1))
-  return redirect('/')
-
-@app.route('/', methods=["POST"])
-def add_player2():
-  game2_name = request.form["name2"]
-  game2_choice = request.form["choice2"]
-  new_player = Player2(game2_name, game2_choice)
-
-  player_2.append(new_player)
-  print('LLLLL', str(player_2))
+  # game_name = request.form["name1"]
+  game_choice = request.form["choice1"]
+  new_player = Player(game_choice)
+  player.append(new_player)
   return redirect('/')
 
 
 @app.route('/game')
 def game():
-  if player1.choice == player2.choice:
-    return f"Player1 choice:{player1.choice}\n,Player2 choice: {player2.choice},\n,This is a draw"
+  import random
+  game_list = ['Rock', 'Paper', 'Scissors']
+  computer_choice = random.choice(game_list)
+  if player == computer_choice:
+    return f"Player1 choice:{player}: {computer_choice}, Computer choice \nThis is a draw"
 
-  elif player1.choice == 'Rock':
-    if player2.choice == 'Scissors':
-      return f"{player1.name}: Player1 is the Winner!!"
+  elif player == 'Rock':
+    if computer_choice == 'Scissors':
+      return f"{player} Player1 is the Winner!!"
     else:
-      return f"{player2.name}: Player2 is the Winner!!"
+      return f"{computer_choice} Computer win is the Winner!!"
 
-  elif player1.choice == "Paper":
-    if player2.choice == "Rock":
-      return f"{player1.name}: Player1 is the Winner!!"
+  elif player == "Paper":
+    if computer_choice == "Rock":
+      return f"{player}: Player1 is the Winner!!"
     else:
-      return f"{player2.name}: Player2 is the Winner!!"
+      return f"{computer_choice} Computer win is the Winner!!"
 
-  elif player1.choice == 'Rock':
-    if player2.choice == 'Scissors':
-      return f"{player1.name}:  Player1 is the Winner!!"
+  elif player == 'Rock':
+    if computer_choice == 'Scissors':
+      return f"{player}:  Player1 is the Winner!!"
     else:
-      return f" {player2.name}: Player2 is the Winner!!"
+      return f"{computer_choice} Computer winsis the Winner!!"
   else:
     return "Wrong Command!"
 
--- app/models/game.py
+++ app/models/game.py
@@ -1,7 +1,6 @@
 from app.models.player import *
 
-player1 = Player1('David', 'Rock')
-player2 = Player2('James', 'Paper')
 
-player_1 = [player1]
-player_2 = [player2]
+# players = Player('Rock')
+
+player = []
